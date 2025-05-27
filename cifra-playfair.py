ed7177feb71f4cb6a51d0dc353c8524f74059df2
DevPedrin <devhenrik09@gmail.com>
2025-05-27 12:01:12 +0000

    pequenas alterações

--- cifra-playfair.py
+++ cifra-playfair.py
@@ -1,15 +1,36 @@
 # Trabalho Cifra Playfair - Pedro Henrque Pereira
 
 alfabeto = list("ABCDEFGHIJLMNOPQRSTUVWXYZ")
-word = 'ATACAR'
-word = "".join(dict.fromkeys(word))
+chave = input("Digite a chave: ")
 
-for char in word:
-    indice = alfabeto.index(char)
-    alfabeto.pop(indice)
 
-res = ''.join(list(word) + alfabeto)
-matriz = [list(res[i*5:(i+1)*5]) for i in range(5)]
+def preparar_chave(chave):
+    chave = chave.upper() 
+    chave = "".join(dict.fromkeys(chave)) # Remove letras repetidas
+    return chave
+
+chave = preparar_chave(chave)
+print(chave)
+
+def matriz(chave):
+    for char in chave:
+        indice = alfabeto.index(char)
+        alfabeto.pop(indice) # remove as letras que ja tem na chave
+
+    res = ''.join(list(chave) + alfabeto)
+    matriz = [list(res[i*5:(i+1)*5]) for i in range(5)]
+    return matriz
+
+matriz = matriz(chave)
+
+def preparar_texto(texto):
+    pass
+
+def criptografar(chave, texto):
+    pass
+
+
+
 
 #print(matriz)
 for linha in matriz:
